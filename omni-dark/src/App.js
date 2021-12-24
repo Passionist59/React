4a3dc32f880af36ba69749a5013ae611cbbcdb55
KingdomParadise <ericlee.apollo@gmail.com>
2021-12-24 03:40:55 -0800

    Updated new

--- omni-dark/src/App.js
+++ omni-dark/src/App.js
@@ -1,15 +1,12 @@
-import logo from './logo.svg';
+import React from "react";
+import Content from './content/content';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-      </header>
-    </div>
+    <>
+        <Content />
+    </>
   );
 }
 
---
+++ omni-dark/src/content/content.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Content() {
+    return (
+        <div>
+            dlkjgldfklfkdg
+        </div>
+    )
+}
+
+export default Content;
